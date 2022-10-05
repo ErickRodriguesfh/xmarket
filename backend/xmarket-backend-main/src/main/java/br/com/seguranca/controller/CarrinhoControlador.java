213a92f87ec96ed10090164b90104d01dcdf22c9
Matheus Cauã Rocha Porto <matheuscrp1@hotmail.com>
2022-10-05 15:05:22 -0300

    Response Entity Carrinho Controller - Carrinho Servicos

--- backend/xmarket-backend-main/src/main/java/br/com/seguranca/controller/CarrinhoControlador.java
+++ backend/xmarket-backend-main/src/main/java/br/com/seguranca/controller/CarrinhoControlador.java
@@ -2,15 +2,21 @@ package br.com.seguranca.controller;
 
 
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import br.com.seguranca.dto.CarrinhoDTO;
 import br.com.seguranca.model.Carrinho;
 import br.com.seguranca.services.CarrinhoServico;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @CrossOrigin("*")
 @RestController
@@ -25,26 +31,32 @@ public class CarrinhoControlador {
     public ResponseEntity<Carrinho> AdicionarItemAoCarrinho(@PathVariable("idProduto") Long idProduto,
                                                             @PathVariable("idUsuario") Long idUsuario,
                                                             @PathVariable("quantidade")Integer quantidade){
-
         carrinhoServico.adicionarCarrinho(idProduto,idUsuario,quantidade);
-        return ResponseEntity.status(HttpStatus.OK).build();
-
+        return ResponseEntity.status(201).build();
     }
 
     @GetMapping("/exibirCarrinho/{idUsuario}")
     public ResponseEntity<List<CarrinhoDTO>> buscarCarrinho(@PathVariable("idUsuario") Long id){
-
        List <CarrinhoDTO>  carrinho = carrinhoServico.buscarCarrinho(id);
-        return ResponseEntity.ok().body(carrinho);
+       if(!carrinho.isEmpty()) {
+    	   return ResponseEntity.status(200).body(carrinho);
+       }else {
+    	   return ResponseEntity.status(204).build();
+       }
 
     }
 
+    
+    
+    //Diogo Implementou no Banco!!!! ///
     @DeleteMapping("/removerItem/{idProduto}/{idCliente}/{quantidade}")
-    public String removerItemCarrinho(@PathVariable("idProduto") Long idProduto, @PathVariable("idCliente") Long idCliente,
+    public ResponseEntity<CarrinhoDTO> removerItemCarrinho(@PathVariable("idProduto") Long idProduto, @PathVariable("idCliente") Long idCliente,
                                        @PathVariable("quantidade") Integer quantidadeProduto ){
-
-        carrinhoServico.removerProduto(idProduto, idCliente, quantidadeProduto);
-        return "Produto removido com sucesso";
+    	if(carrinhoServico.removerProduto(idProduto, idCliente, quantidadeProduto)) {
+    		return ResponseEntity.status(200).build();
+    	}else {
+    		return ResponseEntity.status(400).build();
+    	}
     }
 
 
